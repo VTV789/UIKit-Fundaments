001b116172638fde48f48f9435d8f435d7796aaa
Vinh Thanh Vu <vtv@vinh.com>
2015-10-11 01:51:43 -0400

    L1: Outlets & Actions

---
+++ Click/Click/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Click
+//
+//  Created by Vinh Vu on 10/11/15.
+//  Copyright © 2015 bhag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+    }
+}
+
