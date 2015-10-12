6afb89421cae6eebb5fc4a7318b405c9fcdf7ab6
Vinh Thanh Vu <vtv@vinh.com>
2015-10-11 23:29:07 -0400

    Adding a click button

--- Click/Click/ViewController.swift
+++ Click/Click/ViewController.swift
@@ -11,11 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Label
         let label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
         
         self.view.addSubview(label)
+        
+        //Button 
+        let button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
     }
 }
 
